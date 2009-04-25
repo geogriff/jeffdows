872352dc08095204b8a586353bd3b887bb782076
Jessa <git@jessa0.com>
2009-04-24 21:17:57 -0700

    x86/core: fixed putchar scrolling bug

--- arch/x86/core/printf.c
+++ arch/x86/core/printf.c
@@ -8,19 +8,21 @@ static int vga_ypos=0, vga_xpos=0;
 void putchar(char c) {
   if (c == '\n') {
     vga_ypos++;
-    vga_xpos = 0;
 
     // scroll if necessary
     if (vga_ypos >= vga_rows) {
-      for (vga_ypos = 1; vga_ypos < vga_rows; vga_ypos++)
+      for (vga_ypos = 0; vga_ypos < vga_rows-1; vga_ypos++)
         for (vga_xpos = 0; vga_xpos < vga_cols; vga_xpos++)
-          *((short *) VGA_BASE + (vga_ypos-1) * vga_cols + vga_xpos) = 
-            *((short *) VGA_BASE + vga_ypos * vga_cols + vga_xpos);
-          
+          *((short *) VGA_BASE + vga_ypos * vga_cols + vga_xpos) = 
+            *((short *) VGA_BASE + (vga_ypos+1) * vga_cols + vga_xpos);
+      // now clear last line
+      for (vga_xpos = 0; vga_xpos < vga_cols; vga_xpos++)
+        *((short *) VGA_BASE + vga_ypos * vga_cols + vga_xpos) = 0;
     }
+    vga_xpos = 0;
   } else if (c != '\r') {
-    *((char *) VGA_BASE + vga_ypos * vga_cols + 2 * vga_xpos) = c;
-    *((char *) VGA_BASE + vga_ypos * vga_cols + 2 * vga_xpos + 1) = 7;
+    *((char *) VGA_BASE + 2*(vga_ypos * vga_cols + vga_xpos)) = c;
+    *((char *) VGA_BASE + 2*(vga_ypos * vga_cols + vga_xpos)+1) = 7;
     
     // increment current position and do newline if necessary
     if (++vga_xpos >= vga_cols)
