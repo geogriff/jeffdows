cae33f6612f5322dff79634f9f8b0f67c5ff07b9
Jessa <git@jessa0.com>
2009-05-06 19:04:23 -0700

    x86/mem: changed SDT_ defines to members of enum descriptor_type

--- arch/x86/mem/segment.h
+++ arch/x86/mem/segment.h
@@ -30,10 +30,44 @@
    currently, these structs are written the gcc way, which is least significant
    bit first (on little endian machines). note that byte ordering is the same */
 
+// descriptor types
+typedef enum descriptor_type {
+  SDT_NULL = 0b00000,
+  SDT_TSS_AVAIL_16 = 0b00001,
+  SDT_LDT = 0b00010,
+  SDT_TSS_BUSY_16 = 0b00011,
+  SDT_CALL_GATE_16 = 0b00100,
+  SDT_TASK_GATE = 0b00101,
+  SDT_INT_GATE_16 = 0b00110,
+  SDT_TRAP_GATE_16 = 0b00111,
+  SDT_TSS_AVAIL_32 = 0b01001,
+  SDT_TSS_BUSY_32 = 0b01011,
+  SDT_CALL_GATE_32 = 0b01100,
+  SDT_INT_32 = 0b01110,
+  SDT_TRAP_GATE_32 = 0b01111,
+
+  SDT_DATA = 0b10000,
+  SDT_DATA_A = 0b10001,
+  SDT_DATA_W = 0b10010,
+  SDT_DATA_WA = 0b10011,
+  SDT_DATA_E = 0b10100,
+  SDT_DATA_EA = 0b10101,
+  SDT_DATA_EW = 0b10110,
+  SDT_DATA_EWA = 0b10111,
+  SDT_CODE = 0b11000,
+  SDT_CODE_A = 0b11001,
+  SDT_CODE_R = 0b11010,
+  SDT_CODE_RA = 0b11011,
+  SDT_CODE_C = 0b11100,
+  SDT_CODE_CA = 0b11101,
+  SDT_CODE_CR = 0b11110,
+  SDT_CODE_CRA = 0b11111
+} descriptor_type_t;
+
 typedef struct segment_descriptor {
   unsigned limit_low:16;        // segment limit (low 16 bits)
   unsigned base_low:24;         // segment base address (low 24 bits)
-  unsigned type:5;              // descriptor type
+  descriptor_type_t type:5;     // descriptor type
   unsigned dpl:2;               // descriptor privilege level
   unsigned present:1;           // segment present in memory
   unsigned limit_high:4;        // segment limit (high 4 bits)
@@ -44,44 +78,12 @@ typedef struct segment_descriptor {
   unsigned base_high:8;         // segment base address (high 8 bits)
 } PACK segment_descriptor_t;
 
-// descriptor types
-#define SDT_NULL            0b00000
-#define SDT_TSS_AVAIL_16    0b00001
-#define SDT_LDT             0b00010
-#define SDT_TSS_BUSY_16     0b00011
-#define SDT_CALL_GATE_16    0b00100
-#define SDT_TASK_GATE       0b00101
-#define SDT_INT_GATE_16     0b00110
-#define SDT_TRAP_GATE_16    0b00111
-#define SDT_TSS_AVAIL_32    0b01001
-#define SDT_TSS_BUSY_32     0b01011
-#define SDT_CALL_GATE_32    0b01100
-#define SDT_INT_32          0b01110
-#define SDT_TRAP_GATE_32    0b01111
-
-#define SDT_DATA            0b10000
-#define SDT_DATA_A          0b10001
-#define SDT_DATA_W          0b10010
-#define SDT_DATA_WA         0b10011
-#define SDT_DATA_E          0b10100
-#define SDT_DATA_EA         0b10101
-#define SDT_DATA_EW         0b10110
-#define SDT_DATA_EWA        0b10111
-#define SDT_CODE            0b11000
-#define SDT_CODE_A          0b11001
-#define SDT_CODE_R          0b11010
-#define SDT_CODE_RA         0b11011
-#define SDT_CODE_C          0b11100
-#define SDT_CODE_CA         0b11101
-#define SDT_CODE_CR         0b11110
-#define SDT_CODE_CRA        0b11111
-
 typedef struct gate_descriptor {
   unsigned off_low:16;          // gate segment offset (low 16 bits)
   unsigned segment:16;          // gate segment number
   unsigned param_count:5;       // number of word-sized parameters of gate
   unsigned reserved:3;          // reserved (should be 0)
-  unsigned type:5;              // descriptor type
+  descriptor_type_t type:5;     // descriptor type
   unsigned dpl:2;               // descriptor privelege level
   unsigned present:1;           // gate present in memory
   unsigned off_high:16;         // segment offset of gate (high 16 bits)
@@ -100,6 +102,7 @@ typedef union descriptor {
   nonpresent_descriptor_t nonpresent;
 } descriptor_t;
 
+// dont use bitfield here, or PACK will not align 'base' properly
 typedef struct pseudo_segment_descriptor {
   unsigned short limit;         // descriptor limit
   unsigned long base;           // descriptor base address
