75979070c3360268b0a4690d6f70da8c6aa4ff12
Jessa <git@jessa0.com>
2009-06-17 22:53:12 -0700

    init: added preliminary init_{mem,pmem} code

---
+++ init/mem.c
@@ -0,0 +1,44 @@
+#include <page.h>
+
+void init_mem(phys_mmap_t *phys_mmap) {
+  phys_addr_t pmem_pages = NULL;
+  phys_addr_t vmem_pages = NULL;
+
+  for (phys_mmap_t *mmap = &phys_mmap[0]; !(mmap->limit == 0 && 
+                                            mmap->base_addr == 0); mmap++) {
+    // align mmap to page boundaries
+    mmap->base_addr = PAGE_CEIL(mmap->base_addr);
+    mmap->limit = PAGE_FLOOR(mmap->limit);
+
+    // find a suitable spot for the pmem allocator data
+    if (mmap->limit >= pmem_page_count * PAGE_SIZE) {
+      pmem_pages = mmap->base_addr;
+      mmap->base_addr += pmem_page_count * PAGE_SIZE;
+      mmap->limit -= pmem_page_count * PAGE_SIZE;
+    }
+
+    // calculate total number of pages in system
+    page_count += mmap->limit / PAGE_SIZE;
+  }
+
+  for (phys_mmap_t *mmap = &phys_mmap[0]; !(mmap->limit == 0 && 
+                                            mmap->base_addr == 0); mmap++) {
+    // find a suitable spot for the vmem allocator data
+    if (mmap->limit >= page_count * sizeof(page_t)) {
+      vmem_pages = mmap->base_addr;
+      mmap->base_addr += page_count * sizeof(page_t);
+      mmap->limit -= page_count * sizeof(page_t);
+      break;
+    }
+  }
+
+  if (pmem_pages == NULL || vmem_pages == NULL) {
+    //panic
+  }
+
+  // initialize physical memory allocator
+  init_pmem(pmem_pages);
+
+  // initialize virtual memory allocator
+  init_vmem(vmem_pages);
+}
