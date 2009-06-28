777ae51a232f5ec151626e1fe927409fb0cbb667
Jessa <git@jessa0.com>
2009-06-28 15:18:26 -0700

    mem: implemented pmem_get_page

--- mem/pmem.c
+++ mem/pmem.c
@@ -1,4 +1,5 @@
 #include <stddef.h>
+#include <mem/page.h>
 #include <mem/pmem.h>
 
 page_t *pmem_pages;
@@ -8,5 +9,10 @@ pmem_segment_t pmem_segments[MAX_PMEM_SEGMENTS];
 int pmem_segment_count = 0;
 
 page_t *pmem_get_page(phys_addr_t phys_addr) {
+  for (pmem_segment_t *seg = &pmem_segments[0]; 
+       seg < &pmem_segments[pmem_segment_count]; seg++)
+    if (phys_addr > seg->start && phys_addr < seg->start + seg->limit)
+      return &seg->pages[(phys_addr - seg->start) / PAGE_SIZE];
+
   return NULL;
 }
