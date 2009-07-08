305bb083636f8bb8a841644f9944a203e32b5d49
Jessa <git@jessa0.com>
2009-07-07 20:09:25 -0700

    mem: added freelists and implemented pmem_alloc()

--- mem/pmem.c
+++ mem/pmem.c
@@ -8,6 +8,32 @@ int pmem_page_count = 0;
 pmem_segment_t pmem_segments[MAX_PMEM_SEGMENTS];
 int pmem_segment_count = 0;
 
+page_t *freelists[PMEM_FREELIST_COUNT];
+
+void freelist_remove(page_t **freelist, page_t *page) {
+  page_t *cur_page = *freelist;
+  page_t **prev_ptr = freelist;
+  while (cur_page != NULL) {
+    if (cur_page == page) {
+      *prev_ptr = cur_page->next_free;
+      return;
+    }
+    prev_ptr = &cur_page->next_free;
+    cur_page = cur_page->next_free;
+  }
+}
+
+void freelist_push(page_t **freelist, page_t *page) {
+    page->next_free = *freelist;
+    *freelist = page;
+}
+
+page_t *freelist_pop(page_t **freelist) {
+  page_t *page = *freelist;
+  *freelist = (*freelist)->next_free;
+  return page;
+}
+
 page_t *pmem_get_page(phys_addr_t phys_addr) {
   for (pmem_segment_t *seg = &pmem_segments[0]; 
        seg < &pmem_segments[pmem_segment_count]; seg++)
@@ -29,23 +55,71 @@ pmem_segment_t *pmem_get_seg(phys_addr_t phys_addr) {
 void pmem_free(page_t *page) {
   page_t *buddy = pmem_get_page(page->phys_addr ^ PAGE_SIZE);
   pmem_segment_t *seg = pmem_get_seg(page->phys_addr);
-  while (buddy != NULL && 
+
+  // mark page as free and put on freelist
+  page->order = 0;
+  freelist_push(&freelists[page->order], page);
+
+  // merge with other pages as much as possible
+  while (page->order < MAX_PAGE_ORDER && 
+         buddy != NULL && 
          buddy->phys_addr >= seg->start &&
          buddy->phys_addr < seg->start + seg->limit &&
          buddy->order == page->order) {
-    // we want first page struct of the area 
+    // remove pages from freelist
+    freelist_remove(&freelists[page->order], page);
+    freelist_remove(&freelists[page->order], buddy);
+
+    // we really want first page struct of the area 
     if (page->phys_addr & (PAGE_SIZE << page->order)) {
       page_t *temp = page;
       page = buddy;
       buddy = temp;
     }
+    // merge the two pages
     buddy->order = PAGE_ORDER_INUSE;
     page->order++;
+
+    // add the new area to freelist
+    freelist_push(&freelists[page->order], page);
 
     buddy = pmem_get_page(page->phys_addr ^ (PAGE_SIZE << page->order));
   }
 }
 
 page_t *pmem_alloc() {
+  int order = 0;
+
+  // find lowest order freelist with available pages
+  while (freelists[order] == NULL) {
+    order++;
+    if (order > MAX_PAGE_ORDER) {
+      // panic - out of memory
+    }
+  }
+
+  // split free areas until we have a single page
+  while (order > 0) {
+    page_t *first_page = freelist_pop(&freelists[order]);
+
+    order--;
+
+    page_t *buddy_page = pmem_get_page(first_page->phys_addr ^ 
+                                       (PAGE_SIZE << order));
+
+    if (buddy_page != NULL) {
+      buddy_page->order = order;
+      freelist_push(&freelists[order], buddy_page);
+    }
+    
+    first_page->order = order;
+    freelist_push(&freelists[order], first_page);
+  }
+
+  // get first available page
+  page_t *page = freelist_pop(&freelists[order]);
+  // mark page as used
+  page->order = PAGE_ORDER_INUSE;
   
+  return page;
 }
--- mem/pmem.h
+++ mem/pmem.h
@@ -6,11 +6,14 @@
 #define MAX_PMEM_SEGMENTS 128
 #define MAX_PAGE_ORDER 12
 
+#define PMEM_FREELIST_COUNT (MAX_PAGE_ORDER)
+
 #define PAGE_ORDER_INUSE (MAX_PAGE_ORDER+1)
 
 typedef struct page {
   phys_addr_t phys_addr;
   int order;
+  struct page *next_free;
 } page_t;
 
 typedef struct pmem_segment {
@@ -25,6 +28,12 @@ extern int pmem_page_count;
 extern pmem_segment_t pmem_segments[MAX_PMEM_SEGMENTS];
 extern int pmem_segment_count;
 
+void freelist_remove(page_t **freelist, page_t *page);
+void freelist_push(page_t **freelist, page_t *page);
+page_t *freelist_pop(page_t **freelist);
 page_t *pmem_get_page(phys_addr_t phys_addr);
+pmem_segment_t *pmem_get_seg(phys_addr_t phys_addr);
+void pmem_free(page_t *page);
+page_t *pmem_alloc();
 
 #endif /* _MEM_PMEM_H */
