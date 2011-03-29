ea5a7237562d81c405423941c8bcf4f2a00f3bf8
Jessa <git@jessa0.com>
2011-03-29 13:26:25 -0700

    handle allocation failures in kmalloc/kfree test

--- init/main.c
+++ init/main.c
@@ -11,12 +11,20 @@ void main() {
   while(1) {
     for (int i = 0; i < 1000; i++) {
       bufs[i] = kmalloc(sizeof(char) * (i + 1));
+      if (bufs[i] == NULL) {
+	break;
+      }
+
       for (int j = 0; j < i; j++) {
 	bufs[i][j] = 'a' + (j % ('z' - 'a' + 1));
       }
       bufs[i][i] = 0;
     }
-    for (int i = 0; i < 1000; i++) {
+    for (int i = 0; i < 1000; i--) {
+      if (bufs[i] == NULL) {
+	puts("allocation failed!");
+	break;
+      }
       puts(bufs[i]);
       kfree(bufs[i]);
     }
