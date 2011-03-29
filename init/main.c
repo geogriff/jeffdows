41c022bd5f4fc9de2bf336d1bb288e2de80ae491
Jessa <git@jessa0.com>
2011-03-29 13:19:40 -0700

    fix output of kmalloc/kfree test run kmalloc/kfree test on an infinite loop

--- init/main.c
+++ init/main.c
@@ -8,17 +8,18 @@ void main() {
   init();
 
   char *bufs[1000];
-  for (int i = 0; i < 1000; i++) {
-    bufs[i] = kmalloc(sizeof(char) * (i + 1));
-    for (int j = 0; j < i; j++) {
-      bufs[i][j] = 'a' + (j % ('z' - 'a'));
+  while(1) {
+    for (int i = 0; i < 1000; i++) {
+      bufs[i] = kmalloc(sizeof(char) * (i + 1));
+      for (int j = 0; j < i; j++) {
+	bufs[i][j] = 'a' + (j % ('z' - 'a' + 1));
+      }
+      bufs[i][i] = 0;
     }
-    bufs[i][i] = 0;
-  }
-  for (int i = 0; i < 1000; i++) {
-    puts(bufs[i]);
-    puts(" ");
-    kfree(bufs[i]);
+    for (int i = 0; i < 1000; i++) {
+      puts(bufs[i]);
+      kfree(bufs[i]);
+    }
   }
 
   while(1) {
