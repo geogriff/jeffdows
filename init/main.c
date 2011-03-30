3234dc9e7b583591976069e21fa595e13ff29ad4
Jessa <git@jessa0.com>
2011-03-30 07:26:51 -0700

    fill up memory with objects of increasing size and free them to test  kmalloc/kfree

--- init/main.c
+++ init/main.c
@@ -14,30 +14,28 @@ void main() {
   puts("booting OS................\n");
   init();
 
-  struct kmalloc_test_buf *bufs;
-  bool alloc_failed = false;
-  while (!alloc_failed) {
-    for (size_t size = 16; size <= 2048; size *= 2) {
+  for (size_t size = 16; size <= 2048; size *= 2) {
+    struct kmalloc_test_buf *bufs = NULL;
+    while (true) {
       struct kmalloc_test_buf *buf = kmalloc(size);
       if (buf == NULL) {
 	puts("allocation failed!");
-	alloc_failed = true;
 	break;
       }
       buf->next = bufs;
       bufs = buf;
-
+      
       int text_size = (size - sizeof(struct kmalloc_test_buf)) / sizeof(char);
       for (int j = 0; j < text_size - 1; j++) {
 	buf->text[j] = 'a' + (j % ('z' - 'a' + 1));
       }
       buf->text[text_size - 1] = 0;
     }
-  }
-  // free all buffers
-  while (bufs != NULL) {
-    struct kmalloc_test_buf *buf = bufs;
-    bufs = bufs->next;
-    kfree(buf);
+    // free all buffers
+    while (bufs != NULL) {
+      struct kmalloc_test_buf *buf = bufs;
+      bufs = bufs->next;
+      kfree(buf);
+    }
   }
 }
