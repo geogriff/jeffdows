c79c248b700636954ce7ddac659335589104a347
Jessa <git@jessa0.com>
2011-03-29 18:34:11 -0700

    use c99 int and bool types in page.h

--- arch/x86/mem/page.h
+++ arch/x86/mem/page.h
@@ -13,45 +13,48 @@
 
 #ifndef __ASM__
 
-#include <types.h>
 #include <mem/segment.h>
+#include <compiler.h>
+#include <stdbool.h>
+#include <stdint.h>
 
 /* IMPORTANT!! the ordering of these bits in each byte is a compiler choice!
    currently, these structs are written the gcc way, which is least significant
-   bit first (on little endian machines). note that byte ordering is the same */
+   bit first (on little endian machines). note that byte ordering is the
+   same */
 
 typedef struct pde_bigpage {
-  unsigned present:1;           // page present in memory (should be 1)
-  unsigned writable:1;          // writable
-  unsigned user:1;              // user accessable
-  unsigned write_through:1;     // enable write-through caching of this page
-  unsigned cache_disable:1;     // disable caching of this pde
-  unsigned accessed:1;          // page has been read from
-  unsigned dirty:1;             // page has been written to
-  unsigned bigpage:1;           // pde points to a 4mb page (should be 1)
-  unsigned global:1;            // global pde (cache even when pagedir changes)
-  unsigned :3;                  // unused
-  unsigned pat:1;               // page attribute table bit
-  unsigned reserved:9;          // reserved (should be 0)
-  unsigned base_addr:10;        // base address of page (high 10 bits)
+  bool present:1;           // page present in memory (should be 1)
+  bool writable:1;          // writable
+  bool user:1;              // user accessable
+  bool write_through:1;     // enable write-through caching of this page
+  bool cache_disable:1;     // disable caching of this pde
+  bool accessed:1;          // page has been read from
+  bool dirty:1;             // page has been written to
+  bool bigpage:1;           // pde points to a 4mb page (should be 1)
+  bool global:1;            // global pde (cache even when pagedir changes)
+  uint8_t :3;               // unused
+  bool pat:1;               // page attribute table bit
+  uint16_t reserved:9;      // reserved (should be 0)
+  uint16_t base_addr:10;    // base address of page (high 10 bits)
 } PACK pde_bigpage_t;
 
 typedef struct pde_pagetable {
-  unsigned present:1;           // page table present in memory (should be 1)
-  unsigned writable:1;          // writable
-  unsigned user:1;              // user accessable
-  unsigned write_through:1;     // enable write-through caching of page table
-  unsigned cache_disable:1;     // disable caching of page table
-  unsigned accessed:1;          // page table has been read from
-  unsigned :1;                  // unused
-  unsigned bigpage:1;           // pde points to a 4mb page (should be 0)
-  unsigned :4;                  // unused
-  unsigned base_addr:20;        // base address of page table (high 20 bits)
+  bool present:1;           // page table present in memory (should be 1)
+  bool writable:1;          // writable
+  bool user:1;              // user accessable
+  bool write_through:1;     // enable write-through caching of page table
+  bool cache_disable:1;     // disable caching of page table
+  bool accessed:1;          // page table has been read from
+  bool :1;                  // unused
+  bool bigpage:1;           // pde points to a 4mb page (should be 0)
+  uint8_t :4;               // unused
+  uint32_t base_addr:20;    // base address of page table (high 20 bits)
 } PACK pde_pagetable_t;
 
 typedef struct pde_nonpresent {
-  unsigned present:1;       // page/page table present in memory (should be 0)
-  unsigned :31;             // unused
+  bool present:1;       // page/page table present in memory (should be 0)
+  uint32_t :31;         // unused
 } PACK pde_nonpresent_t;
 
 typedef union pde {
@@ -61,22 +64,22 @@ typedef union pde {
 } pde_t;
 
 typedef struct pte_present {
-  unsigned present:1;           // page present in memory (should be 1)
-  unsigned writable:1;          // writable
-  unsigned user:1;              // user accessable
-  unsigned write_through:1;     // enable write-through caching of page
-  unsigned cache_disable:1;     // disable caching of this page
-  unsigned accessed:1;          // page has been read from
-  unsigned dirty:1;             // page has been written to
-  unsigned pat:1;               // page attribute table bit
-  unsigned global:1;            // global pte (cache even when pagedir changes)
-  unsigned :3;                  // unused
-  unsigned base_addr:20;
+  bool present:1;           // page present in memory (should be 1)
+  bool writable:1;          // writable
+  bool user:1;              // user accessable
+  bool write_through:1;     // enable write-through caching of page
+  bool cache_disable:1;     // disable caching of this page
+  bool accessed:1;          // page has been read from
+  bool dirty:1;             // page has been written to
+  bool pat:1;               // page attribute table bit
+  bool global:1;            // global pte (cache even when pagedir changes)
+  uint8_t :3;               // unused
+  uint32_t base_addr:20;    // base address of the page
 } PACK pte_present_t;
 
 typedef struct pte_nonpresent {
-  unsigned present:1;           // page present in memory (should be 0)
-  unsigned :31;                 // unused
+  bool present:1;           // page present in memory (should be 0)
+  uint32_t :31;             // unused
 } PACK pte_nonpresent_t;
 
 typedef union pte {
